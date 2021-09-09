732f8fe5f9559e09c3192f38ca35adc621ed6456
Cipppp <pirvuciprian37@gmail.com>
2021-09-09 11:22:32 +0300

    Backup

--- src/components/QuestionSection/QuestionSection.js
+++ src/components/QuestionSection/QuestionSection.js
@@ -1,26 +1,77 @@
 import React, { useState } from 'react';
-import firebaseAuth from '../../firebase';
 import scrollTo from 'gatsby-plugin-smoothscroll';
+import firebaseAuth from '../../firebase';
+import { getDatabase, ref, set } from 'firebase/database';
+import { useAuth } from '../contexts/AuthContext';
 
 const QuestionSection = ({ rightSection, text, id, done }) => {
     const [answer, setAnswer] = useState('');
-    const [value, setValue] = useState([]);
+    const { currentUser } = useAuth();
+    var profileRef = '';
+    var seriesRef = '';
+    var groupRef = '';
+    var activitiesRef = '';
+    var userId = id;
 
     const handleOnChange = (e) => {
         setAnswer(e.target.value);
     };
 
-    const createAnswer = () => {
+    const keyPressed = ({ key }) => {
+        // Capture answer on Enter key
+        if (key === 'Enter') {
+            handleClick();
+        }
+    };
+
+    function writeUserData(
+        userId,
+        profileRef,
+        seriesRef,
+        groupRef,
+        activitiesRef
+    ) {
+        const db = getDatabase();
+        console.log(userId);
+        console.log(parseInt(userId));
+        set(ref(db, 'Answers/' + parseInt(userId)), {
+            profile: 'CTI',
+            series: 'CC',
+            group: '313',
+            activities: 'Yes',
+        });
+    }
+
+    const handleClick = () => {
+        // Save search term state to React Hooks
+        console.log(id);
+        if (id === 1) {
+            profileRef = answer;
+            console.log(profileRef);
+        } else if (id === 2) {
+            seriesRef = answer;
+            console.log(seriesRef);
+        } else if (id === 3) {
+            groupRef = answer;
+            console.log(groupRef);
+        } else if (id === 4) {
+            activitiesRef = answer;
+            console.log(activitiesRef);
+        }
         scrollTo('#quiz_' + (id + 1));
-        const answerRef = firebaseAuth.database().ref('Answers');
-        const item = {
-            answer,
-        };
-        answerRef.push(item);
+    };
+
+    const createAnswer = () => {
+        // const answerRef = firebaseAuth.database().ref('Answers');
+        // const item = {
+        //     answer,
+        // };
+        // answerRef.push(item);
     };
 
     return (
         <>
+            {console.log(currentUser.uid)}
             <div className="grid grid-cols-2" id={`quiz_${id}`}>
                 <div
                     className={`h-screen flex justify-center items-center bg-login ${
@@ -33,19 +84,26 @@ const QuestionSection = ({ rightSection, text, id, done }) => {
                             required
                             className="w-full p-5 focus:outline-none focus:shadow-outline rounded-full"
                             onChange={handleOnChange}
+                            onKeyPress={keyPressed}
                         />
 
                         {done ? (
                             <button
                                 className="btn-auth hover:bg-green-300 hover:text-white focus:outline-none"
-                                onClick={createAnswer}
+                                onClick={writeUserData(
+                                    userId,
+                                    profileRef,
+                                    seriesRef,
+                                    groupRef,
+                                    activitiesRef
+                                )}
                             >
                                 Done
                             </button>
                         ) : (
                             <button
                                 className="btn-auth hover:bg-jet hover:text-link focus:outline-none"
-                                onClick={createAnswer}
+                                onClick={handleClick}
                             >
                                 Continue
                             </button>
--- src/components/Status/Status.jsx
+++ src/components/Status/Status.jsx
@@ -5,69 +5,11 @@ import firebase from '../../firebase';
 function Status() {
     const { currentUser } = useAuth();
     const [answers, setAnswers] = useState([]);
-    const [loading, setLoading] = useState(false);
-
-    const ref = firebase.database().ref('Answers');
-    function getAnswers() {
-        ref.on('value', (snapshot) => {
-            let answerList = [];
-            snapshot.forEach((snap) => {
-                answerList.push(snap.val());
-            });
-            setAnswers(answerList);
-        });
-    }
-
-    useEffect(() => {
-        getAnswers();
-    }, []);
-
-    // const ref = firebase.firestore().collection('answers');
-
-    // function getAnswers() {
-    //     setLoading(true);
-    //     ref.onSnapshot((querySnapshot) => {
-    //         const items = [];
-    //         querySnapshot.forEach((doc) => {
-    //             items.push(doc.data());
-    //         });
-    //         setAnswers(items);
-    //         setLoading(false);
-    //     });
-    // }
-
-    // useEffect(() => {
-    //     getAnswers();
-    // }, []);
-
-    // if (loading) {
-    //     return <h1>Loading...</h1>;
-    // }
 
     return (
         <>
             <div className="grid grid-cols-2">
-                <div className="h-screen flex justify-center items-center bg-login col-span-2">
-                    {/* {answers.map((answer) => (
-                        <div key={answer.id}>
-                            <h2>{answer.profile}</h2>
-                            <h2>{answer.series}</h2>
-                            <h2>{answer.group}</h2>
-                            <h2>{answer.volunteer_activities}</h2>
-                        </div>
-                    ))} */}
-
-                    {answers.map((answer) => {
-                        return (
-                            <div key={answer.id}>
-                                <h2>{answer.profile}</h2>
-                                <h2>{answer.series}</h2>
-                                <h2>{answer.group}</h2>
-                                <h2>{answer.volunteer_activities}</h2>
-                            </div>
-                        );
-                    })}
-                </div>
+                <div className="h-screen flex justify-center items-center bg-login col-span-2"></div>
             </div>
         </>
     );
