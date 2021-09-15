cf1f5f41a181b69233511245481123f966b7c1d5
Cipppp <pirvuciprian37@gmail.com>
2021-09-15 17:38:06 +0300

    Added username in the middle of the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, Redirect } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import * as Falcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
@@ -7,20 +7,32 @@ import { SidebarData } from './SidebarData.js';
 import './Navbar.css';
 import { IconContext } from 'react-icons/lib';
 
+import { getDatabase, ref, onValue } from 'firebase/database';
+
 function Navbar() {
     const [error, setError] = useState('');
     const [sidebar, setSidebar] = useState(false);
 
     const showSidebar = () => setSidebar(!sidebar);
-    const { currentUser, logout, username, getUserData } = useAuth();
+    const { currentUser, logout, getUserData } = useAuth();
     const history = useHistory();
     const isOnPage = history.location.pathname === '/login';
 
+    const [username, setUsername] = useState('Loading...');
+
     useEffect(() => {
-        if (currentUser) {
-            getUserData();
+        if (!currentUser) {
+            Redirect('/login');
+        } else {
+            const db = getDatabase();
+            const usernameRef = ref(db, 'Users/' + currentUser.uid);
+            onValue(usernameRef, (snapshot) => {
+                const data = snapshot.val().username;
+                setUsername(data);
+            });
         }
     }, []);
+
     return (
         <>
             <IconContext.Provider value={{ color: '#fff' }}>
@@ -32,6 +44,8 @@ function Navbar() {
                         >
                             Awaree.
                         </Link>
+
+                        <h1 className="text-lg text-white">{username}</h1>
                         <Link to="#" className="menu-bars">
                             <Falcons.FaBars onClick={showSidebar} />
                         </Link>
