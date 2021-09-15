c9d7b5f3bb80e74228a7d1be584b4cf4f1602881
Cipppp <pirvuciprian37@gmail.com>
2021-09-15 07:06:51 +0300

    Updated Sidebar and new pages

---
+++ src/components/Navbar/SidebarData.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+import * as MdIcons from 'react-icons/md';
+import * as SiIcons from 'react-icons/si';
+import * as FiIcons from 'react-icons/fi';
+
+export const SidebarData = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text',
+    },
+    {
+        title: 'Status',
+        path: '/status',
+        icon: <IoIcons.IoIosStats />,
+        cName: 'nav-text',
+    },
+
+    {
+        title: 'Homeworks',
+        path: '/homeworks',
+        icon: <MdIcons.MdWork />,
+        cName: 'nav-text',
+    },
+    {
+        title: 'Subjects',
+        path: '/subjects',
+        icon: <SiIcons.SiGoogleclassroom />,
+        cName: 'nav-text',
+    },
+    {
+        title: 'Log out',
+        path: '/',
+        icon: <FiIcons.FiLogOut />,
+        cName: 'nav-text',
+    },
+];
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div className="home">
+            <h1>Home </h1>
+        </div>
+    );
+}
