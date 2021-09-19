706fd937185afe56e25f0c339a4113eed0db765d
Cipppp <pirvuciprian37@gmail.com>
2021-09-19 20:34:40 +0300

    Google button padding #2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -208,9 +208,9 @@ function Login() {
                             <button
                                 disabled={loading}
                                 onClick={handleGoogleLogin}
-                                className="btn-auth-with hover:bg-jet hover:text-link focus:outline-none "
+                                className="btn-auth-with hover:bg-jet hover:text-link focus:outline-none p-2"
                             >
-                                <GoogleImg className="w-8 mr-4 pt-2 mb-2 text-sm sm:text-base md:text-lg xl:text-xl p-2" />
+                                <GoogleImg className="w-8 mr-4 pt-2 mb-2 text-sm sm:text-base md:text-lg xl:text-xl" />
                                 Continue with Google
                             </button>
                         </div>
